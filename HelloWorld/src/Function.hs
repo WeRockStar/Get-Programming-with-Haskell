f63134221b677fe92ba8e5f9d94c9e36eaed168b
WeRockStar <kotchaphan.m@gmail.com>
2019-05-11 14:19:20 +0700

    Refactor if condition with where clause

--- HelloWorld/src/Function.hs
+++ HelloWorld/src/Function.hs
@@ -1,5 +1,6 @@
-calcChange owed given = if given - owed > 0 
+calcChange owed given = if change > 0 
     then given - owed
     else 0
+    where change = given - owed
 main = do 
     print (calcChange 2 10)
