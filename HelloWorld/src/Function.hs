877e2f0328f828236c9df32949ed3280c4ede379
WeRockStar <kotchaphan.m@gmail.com>
2019-05-11 14:36:43 +0700

    Add more example of function

--- HelloWorld/src/Function.hs
+++ HelloWorld/src/Function.hs
@@ -2,5 +2,12 @@ calcChange owed given = if change > 0
     then given - owed
     else 0
     where change = given - owed
+
+f x = x ^ 3
+
 main = do 
     print (calcChange 2 10)
+    print (f 2)
+    -- Lambda functions (\x -> x) 4
+    let x = (\x -> x) 30
+    print x
