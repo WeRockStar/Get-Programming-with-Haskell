1bcdf4b2a1fb6a4ed3261e97d67cef39b50d24da
WeRockStar <kotchaphan.m@gmail.com>
2019-05-11 13:34:57 +0700

    Refactor to createEmail function

--- HelloWorld/src/ReaderInput.hs
+++ HelloWorld/src/ReaderInput.hs
@@ -3,6 +3,8 @@ main :: IO()
 toPart recipient = "Dear " ++ recipient ++ ",\n"
 bodyPart title = "Thank for buying " ++ title ++ "\n"
 fromPart author = "Thanks, \n" ++ author
+createEmail recipient title author = toPart recipient ++ bodyPart title ++ fromPart author
+
 main = do
     print "Who is the email for?"
     recipient <- getLine
@@ -10,4 +12,4 @@ main = do
     title <- getLine
     print "Who is Author?"
     author <- getLine
-    print (toPart recipient ++ bodyPart title ++ fromPart author)
+    print (createEmail recipient title author)
