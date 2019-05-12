9275e5bd7c21cd1170ff4c5fd1a51c96f29ad975
WeRockStar <kotchaphan.m@gmail.com>
2019-05-12 15:45:46 +0700

    Add basic List

--- List/src/Main.hs
+++ List/src/Main.hs
@@ -1,5 +1,20 @@
-module Main where
 
-main :: IO ()
 main = do
-  putStrLn "hello world"
+  let teams = ["red", "yello", "greed", "orange", "purple"]
+  print (teams)
+  print ("Head: " ++ head teams)
+  let t = (tail teams)
+  print (t)
+  -- Building List
+  print (1:[2,3,4,5])
+  print (1:[])
+  print (1:2:3:4:[5,6])
+  print (['W','e'])
+  print ('W':'e':['R', 'o','c', 'k', 'S', 't', 'a', 'r'])
+  -- concatenate list 
+  print ([1] ++ [2,3])
+  -- Generate ranges of data
+  print ([1..10])
+  print ([1,2.5..10])
+  print ([-10 .. -1])
+  print ([1 .. ])
